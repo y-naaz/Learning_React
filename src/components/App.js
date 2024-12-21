3935438c7f09fa9c0034e37d99d468894254b612
YASMEEN <yasmeennaaz@gmail.com>
2024-12-21 23:31:20 +0530

    removed unnecessary imports

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Header from "./Header";
+// import AddContact from "./AddContact";
+// import ContactList from "./ContactList";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      happy coding!
+    </div>
+);
+}
+
+export default App;
