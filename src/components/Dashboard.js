ce16dc1544d72eae903f7ca1deef4d24e233ed8f
YASMEEN <yasmeennaaz@gmail.com>
2025-01-03 12:25:03 +0530

    updated package.json

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar"; // Import the Navbar component
 import { useNavigate } from "react-router-dom"; // Import useNavigate
-// Remove Logout import from here since it's used inside Navbar
 
 const texts = ["Stock", "Invest", "Portfolio", "TrackPORT"];
 
